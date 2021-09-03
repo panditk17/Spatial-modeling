9f4ed5e1da8696db2b1e36a5fd5b4f99a68d03a4
Karun Pandit <karunpandit@gmail.com>
2021-09-03 18:06:37 -0400

    Add files via upload

---
+++ aggregate_by_fishnet/aggregate_fishnet_fn.R
@@ -0,0 +1,85 @@
+aggregate_fishnet_fn<-function(point_data,map,pixelsize) {
+
+
+# read RCS file with tree data 
+data<-point_data
+
+lob_nat<-data
+lob_nat2<-lob_nat[c("plot","long","lat")]
+lob_nat2$presence<-1
+coordinates(lob_nat2)<-~long+lat
+
+projlatlon<-"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+proj4string(lob_nat2)=CRS(projlatlon) # set it to lat-long
+
+
+lob_nat3 = spTransform(lob_nat2,proj4)
+
+
+
+projection(lob_nat3)
+extent(lob_nat3)
+
+ccc<-projection(map)
+
+fishnet.r <- raster(extent(map))
+res(fishnet.r) <- pixelsize
+
+proj4string(fishnet.r) <- CRS("+proj=laea +lat_0=0 +lon_0=-80 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
+
+fishnet <- rasterToPolygons(fishnet.r)
+
+plot(fishnet)
+
+
+fishnet@data$id<-as.numeric(rownames(fishnet@data))
+
+fishnet_latlon<-spTransform(fishnet, proj_latlon)
+
+centroid_latlon <- as.data.frame(coordinates(fishnet_latlon))
+centroid_proj <- as.data.frame(coordinates(fishnet))
+
+centroid_both<-merge(centroid_latlon,centroid_proj, by="row.names")
+
+# 
+# centroid_id <- as.data.frame(fishnet_latlon@data$id)
+# 
+# centroid2<-merge(centroid_id,centroid_latlon, by="row.names")
+# 
+# fishnet_latlon@data<-st_join(fishnet_latlon,centroid_latlon)
+
+fishnet@data <- merge(x=fishnet@data,y=centroid_both,by.x="id",
+                          by.y="Row.names", all.x=TRUE)
+
+
+# fishnet2<-st_as_sf(fishnet)
+# plot(fishnet2)
+# 
+# 
+# 
+# points_new<-data.frame(identify(lob_nat3,fishnet2))
+
+
+# pnts <- lob_nat3 %>% mutate(
+#   intersection = as.integer(st_intersects(geometry, fishnet2))
+#   , area = if_else(is.na(intersection), '', fishnet2$id[intersection])
+# ) 
+
+
+point_dd<-data.frame(lob_nat3)
+points_nn<-over(lob_nat3,fishnet)
+points_id<-data.frame(lob_nat3$plot)
+points2<-merge(points_id,points_nn,by="row.names")
+
+points3<-points2[!is.na(points2$id),]
+
+points4<-cbind(points3[,1:3],points3[,5:8])
+
+colnames(points4)<-c("rowid","plot","fishnetid","fishlong1","fishlat1",
+                     "fishlong2","fishlat2")
+
+
+write.csv(points4,"data_point_fishnet.csv")
+
+
+}
---
+++ aggregate_by_fishnet/main_script.R
@@ -0,0 +1,70 @@
+# code to identify repeated measurements (trees and plots) using FIA data
+# with fusiform rust from 2013 to 2019 in Southern US
+
+setwd("C:/Karuns_documents/pine_disease/survival_fishnet")
+
+rm(list=ls())
+
+library(dplyr)
+library(reshape)
+library(ggplot2)
+
+library(dplyr)
+library(reshape)
+library(ggplot2)
+library(USAboundaries)
+library(sf)
+library(gridExtra)
+library(rgdal)
+library(sp)
+library(raster)
+
+# 
+# st_list = c("Alabama", "Arkansas","Florida",
+#             "Georgia", "Kentucky", "Louisiana",
+#              "Mississippi", "North Carolina", "Oklahoma", 
+#             "South Carolina", "Tennessee",
+#             "Texas", "Virginia")
+
+st_list = c("Alabama", "Arkansas", "Connecticut", "Delaware", "Florida",
+            "Georgia", "Illinois", "Indiana",
+            "Iowa", "Kansas", "Kentucky", "Louisiana",
+            "Maine", "Maryland","Massachusetts",  "Michigan",
+            "Minnesota", "Mississippi", "Missouri","Nebraska",
+            "New Hampshire","New Jersey",
+            "New York", "North Carolina", "North Dakota",
+            "Ohio", "Oklahoma", "Pennsylvania",
+            "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+            "Texas", "Vermont","Virginia",
+            "West Virginia", "Wisconsin")
+# 
+# 
+# devtools::install_github("ropensci/USAboundaries")
+# devtools::install_github("ropensci/USAboundariesData")
+
+
+states_contemporary <- us_states(states=st_list)
+plot(st_geometry(states_contemporary))
+title("Southeastern US")
+
+ext1<-extent(states_contemporary)
+proj1<-crs(states_contemporary)
+
+proj_latlon="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+
+ proj3<-"+proj=laea +lat_0=0 +lon_0=-80"
+# 
+ map <- st_transform(states_contemporary, proj3)
+# plot(st_geometry(map))
+
+proj4<-crs(map)
+
+point_data=read.csv("data/fia_coord1.csv")  
+
+pixelsize=100000
+
+
+source("codes/aggregate_fishnet_fn.R")
+
+aggregate_fishnet_fn(point_data,map,pixelsize)
+
